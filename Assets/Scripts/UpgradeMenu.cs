1523da5a49d8feba188888929b27c268e662361b
SuperPupPresent <antfleck07@gmail.com>
2024-11-09 16:28:56 -0500

    Upgrading works, the UI is partially done

--- Assets/Scripts/UpgradeMenu.cs
+++ Assets/Scripts/UpgradeMenu.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UpgradeMenu : MonoBehaviour
 {
-    TreeHive hive;
+    [SerializeField] TreeHive hive;
+    [SerializeField] TextMeshPro[] upgradeKeys;
+    [SerializeField] TextMeshPro[] upgradeCosts;
+    [SerializeField] SpriteRenderer[] upgradeSprites;
     // Start is called before the first frame update
     void Start()
     {
-        
+        upgradeCosts[1].text = "" + 20;
+        upgradeCosts[2].text = "" + 30;
+        upgradeCosts[3].text = "" + 5;
+    }
+
+    private void OnEnable()
+    {
+        upgradeCosts[0].text = "" + (float)hive.currentBeeCapacity * 0.5f;
     }
 
     // Update is called once per frame
