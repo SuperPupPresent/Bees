3ca2c2cb8b52f92931e330c6531ff0ea3c025a9c
ComradeRyn <106786136+ComradeRyn@users.noreply.github.com>
2024-11-09 15:04:54 -0500

    Event System

---
+++ Assets/Scripts/Event Scripts/GameEvent.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent", menuName = "ScriptableObjects/GameEventSO", order = 1)]
+public class GameEvent : ScriptableObject //Acts as the inbetween
+{
+    public List<GameEventListener> listeners = new List<GameEventListener>();
+
+    //Broadcast / Raise event through different method signatures
+
+    public void Raise(Component sender, object data)
+    {
+        Debug.Log("Raise has been called");
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised(sender, data);
+            //Debug.Log("Event has successfully been raised");
+        }
+    }
+
+    //Manage Listeners
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (!listeners.Contains(listener))
+        {
+            listeners.Add(listener);
+        }
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if (listeners.Contains(listener))
+        {
+            listeners.Remove(listener);
+        }
+    }
+}
